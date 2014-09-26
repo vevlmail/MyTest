2708708a94f0af0dac4b3722e2ecd2f8b847d135
vevlmail <vevlmail@mail.ru>
2014-09-26 23:39:15 +0300

    Test 1

---
+++ scalesmanager/start.cpp
@@ -0,0 +1,18 @@
+/*
+ * start.cpp
+ *
+ *  Created on: Sep 20, 2014
+ *      Author: hourse
+ */
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int a = 0;
+	std::cout << "Hello Я" << std::endl;
+	std::cin>>a;
+	return 0;
+}
+
+
+
