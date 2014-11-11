86ea7315853121180ede592f8f99fc05e76d9733
vevl <vevl@MAINSYS7.7.klass>
2014-11-11 18:09:04 +0200

    unit6

--- scalesmanager/start.cpp
+++ scalesmanager/start.cpp
@@ -5,42 +5,21 @@
  *      Author: hourse
  */
 #include <iostream>
-#include <vector>
-#include <fstream>
+const double f1 (const double, int);
 
 using namespace std;
 
 int main(int argc, char **argv) {
 
-	char automobile[50];
-	int year;
-	double a_price, d_price;;
-	ofstream outFile;
-	outFile.open("carinfo.txt");
-	cout<<"Enter the make and model of automobile: ";
-	cin.getline(automobile,50);
-	cout<<"Enter the model year: ";
-	cin>>year;
-	cout<<"Enter the price: ";
-	cin>>a_price;
-	d_price=9.13*a_price;
-
-
-	cout<<fixed;
-	cout.precision(2);
-	cout.setf(ios_base::showpoint);
-	cout<<"the make and model of automobile: "
-	<< automobile << endl;
-	cout<<"the model year: "
-	<<year<<endl;
-	cout<<"the price: "
-	<< d_price << endl;
-	outFile<< fixed;
-	outFile.precision(2);
-	outFile.setf(ios_base::showpoint);
-	outFile<<d_price;
-	outFile.close();
+	const double (*p)(const double, int) = &f1;
+	auto p1 = &p;
+	cout<<&p1<<endl;
+	cout <<(*p1)(3.0, 1)<<endl;
 	std::cin.get();
 	return 0;
 }
 
+const double f1( const double ar, int n)
+{
+	return ar;
+}
