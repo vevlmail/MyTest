283e6b54f9ea18eddcd6fdc6b8088563df83d6b2
vevl <vevl@MAINSYS7.7.klass>
2014-11-18 14:35:42 +0200

    Empty

--- scalesmanager/start.cpp
+++ scalesmanager/start.cpp
@@ -5,21 +5,12 @@
  *      Author: hourse
  */
 #include <iostream>
-const double f1 (const double, int);
+#include <new>
 
-using namespace std;
 
 int main(int argc, char **argv) {
 
-	const double (*p)(const double, int) = &f1;
-	auto p1 = &p;
-	cout<<&p1<<endl;
-	cout <<(*p1)(3.0, 1)<<endl;
 	std::cin.get();
 	return 0;
 }
 
-const double f1( const double ar, int n)
-{
-	return ar;
-}
